ae2d92543693e6b98b5fcf238aa435bd42697cbf
asgerami <amiraman467@gmail.com>
2024-12-10 22:01:06 +0300

    chore: reformatted for cleaner code

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,7 +1,6 @@
 import { Editor } from "./editor";
 import { Toolbar } from "./toolbar";
 
-
 interface DocumentIdPageProps {
   params: Promise<{ documentId: string }>; //NextJS 15
 }
@@ -9,11 +8,12 @@ interface DocumentIdPageProps {
 const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
   const { documentId } = await params;
 
-  return(
-  <div className="min-h-screen bg-[#FAFBFD]">
-    <Toolbar />
-    <Editor />
-    </div>);
+  return (
+    <div className="min-h-screen bg-[#FAFBFD]">
+      <Toolbar />
+      <Editor />
+    </div>
+  );
 };
 
 export default DocumentIdPage;
