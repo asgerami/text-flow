c568d9574484d9e45296274f34740ea105b63cf3
asgerami <amiraman467@gmail.com>
2025-01-30 15:40:08 +0300

    feat: add Inbox component for displaying notifications in the document navbar

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -39,6 +39,7 @@ import { DocumentInput } from "./document-input";
 import { BsFilePdf } from "react-icons/bs";
 import { useEditorStore } from "@/store/use-editor-store";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { Inbox } from "./inbox";
 
 export const Navbar = () => {
   const { editor } = useEditorStore();
@@ -256,6 +257,7 @@ export const Navbar = () => {
       </div>
       <div className="flex gap-3 items-center pl-6">
         <Avatars />
+        <Inbox />
         <OrganizationSwitcher
         afterCreateOrganizationUrl="/"
         afterLeaveOrganizationUrl="/"
